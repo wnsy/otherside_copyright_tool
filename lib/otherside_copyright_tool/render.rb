347b5a54337a539ed7e5fbff626192c97f721caf
Ayesha W <ayesha.wnz@gmail.com>
2020-02-18 16:08:57 +1300

    Removes stray line

--- lib/otherside_copyright_tool/render.rb
+++ lib/otherside_copyright_tool/render.rb
@@ -1,5 +1,4 @@
 module OthersideCopyrightTool
-  class Error < StandardError; end
   class Render
     def self.copyright name, msg
       "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
